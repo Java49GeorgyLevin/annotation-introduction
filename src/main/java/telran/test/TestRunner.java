78426d03c89977672de17aee114c23a574ddfb01
Georgy Levin <g.phyzy@gmail.com>
2023-10-27 22:12:34 +0300

    HW 54: BeforeEach added

--- src/main/java/telran/test/TestRunner.java
+++ src/main/java/telran/test/TestRunner.java
@@ -1,13 +1,15 @@
 package telran.test;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-
+import telran.test.annotation.BeforeEach;
 import telran.test.annotation.Test;
 
 public class TestRunner implements Runnable {
 	private Object testObj;
-	
+	Method[] methods;
+
 	public TestRunner(Object testObj) {
 		super();
 		this.testObj = testObj;
@@ -15,19 +17,23 @@ public class TestRunner implements Runnable {
 
 	@Override
 	public void run() {
-			Class<?> clazz = testObj.getClass();
-			Method[] methods = clazz.getDeclaredMethods();
-			for(Method method: methods) {
-				if(method.isAnnotationPresent(Test.class)) {
-					method.setAccessible(true);
-					try {
-						method.invoke(testObj);
-					} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
-						System.out.println("error: " + e.getMessage());
-					}
+		Class<?> clazz = testObj.getClass();
+		methods = clazz.getDeclaredMethods();
+		invokeAnnotation(BeforeEach.class);
+		invokeAnnotation(Test.class);
+	}
+
+	private void invokeAnnotation(Class<? extends Annotation> clazz) {
+		for (Method method : methods) {
+			if (method.isAnnotationPresent(clazz)) {
+				method.setAccessible(true);
+				try {
+					method.invoke(testObj);
+				} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+					System.out.println("error: " + e.getMessage());
 				}
 			}
-
+		}
 	}
 
 }
